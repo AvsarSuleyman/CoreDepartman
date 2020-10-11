2a9019a75473e072f3613e1da44bc2eb5b4f679d
Süleyman <suleymanavsar1995@gmail.com>
2020-10-11 16:10:54 +0300

    Initial commit

---
+++ Controllers/personelController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreDepartman.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDepartman.Controllers
+{
+    public class personelController : Controller
+    {
+        Context c = new Context();
+
+        public IActionResult Index()
+        {
+            var degerler = c.personels.ToList();
+         
+            var query = from person in c.personels
+                        join dep in c.departmanlars on person.departid equals dep.id
+                        select new personel()
+                        {
+                            ad = person.ad,
+                            soyad = person.soyad,
+                            sehir = person.sehir,
+                            perid = person.perid,
+                            departid = person.departid,
+                            depart = new departmanlar()
+                            {
+                                id = dep.id,
+                                departmanad = dep.departmanad,
+                                personels = null
+                            }
+                        };
+            return View(query.ToList());
+        }
+    }
+}
---
+++ Models/departmanlar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace CoreDepartman.Models
+{
+    public class departmanlar
+    {
+        [Key]
+        public int id { get; set; }
+        public string departmanad { get; set; }
+
+
+        public List<personel> personels { get; set; }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
